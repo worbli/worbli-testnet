a807b2570e61a037d9bf3b42a60c418bb66162a9
Robert DeWilder <rob@worbli.io>
2018-09-08 18:33:11 -0400

    updated endpoints to wireguard IP

---
+++ scripts/create-prod-accounts.sh
@@ -0,0 +1,13 @@
+worbli system newaccount eosio --transfer bp.worbli EOS7Twda1EMGC1FYH2o9d7APfmwZAMtN3e69wQg1CPjBqQSaWH39P --stake-net "5000.0000 WBI" --stake-cpu "100000.0000 WBI" --buy-ram-kbytes 4
+worbli system newaccount eosio --transfer bp2.worbli EOS7Twda1EMGC1FYH2o9d7APfmwZAMtN3e69wQg1CPjBqQSaWH39P --stake-net "5000.0000 WBI" --stake-cpu "100000.0000 WBI" --buy-ram-kbytes 4
+worbli system newaccount eosio --transfer bp3.worbli EOS7Twda1EMGC1FYH2o9d7APfmwZAMtN3e69wQg1CPjBqQSaWH39P --stake-net "5000.0000 WBI" --stake-cpu "100000.0000 WBI" --buy-ram-kbytes 4
+
+worbli system newaccount eosio --transfer eosphere EOS8Cv2bK4NSyqFx34Q4oWf4fgpcJqPR2YdaUndN9pg6XspDLaNZw --stake-net "5000.0000 WBI" --stake-cpu "100000.0000 WBI" --buy-ram-kbytes 4
+
+worblim system newaccount eosio --transfer infinityblk1 EOS7NVEUk8CqT9nKefN5ygiAusyt6hZreHBGq23QhGuJrETiW3fSJ --stake-net "5000.0000 WBI" --stake-cpu "100000.0000 WBI" --buy-ram-kbytes 4
+
+worblim system newaccount eosio --transfer blocksmithio EOS78fHwrxZSki376sJoKanJHgNfGGE5vX3vAUNwBoNFzULbDNPab --stake-net "5000.0000 WBI" --stake-cpu "100000.0000 WBI" --buy-ram-kbytes 4
+
+
+# what happens when we have 2 producers and 1 isn't producing
+# can i recover from this given i have the master key
---
+++ scripts/setprod.sh
@@ -0,0 +1,4 @@
+
+#!/bin/sh
+
+worblim push action eosio setprods "{\"schedule\":[{\"producer_name\":\"bp.worbli\",\"block_signing_key\":\"EOS7Twda1EMGC1FYH2o9d7APfmwZAMtN3e69wQg1CPjBqQSaWH39P\"},{\"producer_name\":\"bp1.worbli\",\"block_signing_key\":\"EOS7Twda1EMGC1FYH2o9d7APfmwZAMtN3e69wQg1CPjBqQSaWH39P\"},{\"producer_name\":\"bp2.worbli\",\"block_signing_key\":\"EOS7Twda1EMGC1FYH2o9d7APfmwZAMtN3e69wQg1CPjBqQSaWH39P\"},{\"producer_name\":\"eosphere\",\"block_signing_key\":\"EOS8Cv2bK4NSyqFx34Q4oWf4fgpcJqPR2YdaUndN9pg6XspDLaNZw\"},{\"producer_name\":\"infinityblk1\",\"block_signing_key\":\"EOS7NVEUk8CqT9nKefN5ygiAusyt6hZreHBGq23QhGuJrETiW3fSJ\"},{\"producer_name\":\"blocksmithio\",\"block_signing_key\":\"EOS78fHwrxZSki376sJoKanJHgNfGGE5vX3vAUNwBoNFzULbDNPab\"}]}" -p eosio@active
