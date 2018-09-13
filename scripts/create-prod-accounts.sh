a871e1f9ab6511f727af5d807a3586f0c4fb49b2
Robert DeWilder <rob@worbli.io>
2018-09-13 10:44:00 -0400

    added eosio.sg details

--- scripts/create-prod-accounts.sh
+++ scripts/create-prod-accounts.sh
@@ -1,13 +1,26 @@
-worbli system newaccount eosio --transfer bp.worbli EOS7Twda1EMGC1FYH2o9d7APfmwZAMtN3e69wQg1CPjBqQSaWH39P --stake-net "5000.0000 WBI" --stake-cpu "100000.0000 WBI" --buy-ram-kbytes 4
-worbli system newaccount eosio --transfer bp2.worbli EOS7Twda1EMGC1FYH2o9d7APfmwZAMtN3e69wQg1CPjBqQSaWH39P --stake-net "5000.0000 WBI" --stake-cpu "100000.0000 WBI" --buy-ram-kbytes 4
-worbli system newaccount eosio --transfer bp3.worbli EOS7Twda1EMGC1FYH2o9d7APfmwZAMtN3e69wQg1CPjBqQSaWH39P --stake-net "5000.0000 WBI" --stake-cpu "100000.0000 WBI" --buy-ram-kbytes 4
+worblim system newaccount eosio --transfer bp.worbli EOS7Twda1EMGC1FYH2o9d7APfmwZAMtN3e69wQg1CPjBqQSaWH39P --stake-net "5000.0000 WBI" --stake-cpu "100000.0000 WBI" --buy-ram-kbytes 4
+worblim system newaccount eosio --transfer bp2.worbli EOS7Twda1EMGC1FYH2o9d7APfmwZAMtN3e69wQg1CPjBqQSaWH39P --stake-net "5000.0000 WBI" --stake-cpu "100000.0000 WBI" --buy-ram-kbytes 4
+worblim system newaccount eosio --transfer bp3.worbli EOS7Twda1EMGC1FYH2o9d7APfmwZAMtN3e69wQg1CPjBqQSaWH39P --stake-net "5000.0000 WBI" --stake-cpu "100000.0000 WBI" --buy-ram-kbytes 4
 
-worbli system newaccount eosio --transfer eosphere EOS8Cv2bK4NSyqFx34Q4oWf4fgpcJqPR2YdaUndN9pg6XspDLaNZw --stake-net "5000.0000 WBI" --stake-cpu "100000.0000 WBI" --buy-ram-kbytes 4
+worblim system newaccount eosio --transfer eosphere EOS8Cv2bK4NSyqFx34Q4oWf4fgpcJqPR2YdaUndN9pg6XspDLaNZw --stake-net "5000.0000 WBI" --stake-cpu "100000.0000 WBI" --buy-ram-kbytes 4
 
 worblim system newaccount eosio --transfer infinityblk1 EOS7NVEUk8CqT9nKefN5ygiAusyt6hZreHBGq23QhGuJrETiW3fSJ --stake-net "5000.0000 WBI" --stake-cpu "100000.0000 WBI" --buy-ram-kbytes 4
 
 worblim system newaccount eosio --transfer blocksmithio EOS78fHwrxZSki376sJoKanJHgNfGGE5vX3vAUNwBoNFzULbDNPab --stake-net "5000.0000 WBI" --stake-cpu "100000.0000 WBI" --buy-ram-kbytes 4
 
+worblim system newaccount eosio --transfer eoscafeblock EOS8anG4qVhtSyh6D5GhbZugKcrnHo4Jcw9b9NBNkJuRPU2vmQ4Qi --stake-net "5000.0000 WBI" --stake-cpu "100000.0000 WBI" --buy-ram-kbytes 4
 
-# what happens when we have 2 producers and 1 isn't producing
-# can i recover from this given i have the master key
+worblim system newaccount eosio --transfer dune1 EOS6hEW7THHrTXg93o9HjyQfH2ne8g6pw88P8SxctKVedcqz1jKuP --stake-net "5000.0000 WBI" --stake-cpu "100000.0000 WBI" --buy-ram-kbytes 4
+
+worblim system newaccount eosio --transfer beyondbtc1 EOS87H8Y1hjZnAaqQa4iCFXVfDEq2EKi1yLHiThA4ketYwtLW54LD --stake-net "5000.0000 WBI" --stake-cpu "100000.0000 WBI" --buy-ram-kbytes 4
+
+worblim system newaccount eosio --transfer eosiodetroit EOS5M9AkrsHoQHUwf7oiu5q5drjgD7WMjTtmJX4Jd13bo9W7Z8cBo --stake-net "5000.0000 WBI" --stake-cpu "100000.0000 WBI" --buy-ram-kbytes 4
+
+worblim system newaccount eosio --transfer eosnewyorkio EOS55qQ1GRvFqn75CyNhnQRP7ZFwu3xBY4Zu5ryawY1tm7Q3UwNYH --stake-net "5000.0000 WBI" --stake-cpu "100000.0000 WBI" --buy-ram-kbytes 4
+
+worblim system newaccount eosio --transfer eosnewyorkio EOS55qQ1GRvFqn75CyNhnQRP7ZFwu3xBY4Zu5ryawY1tm7Q3UwNYH --stake-net "5000.0000 WBI" --stake-cpu "100000.0000 WBI" --buy-ram-kbytes 4
+
+worblim system newaccount eosio --transfer eosmetal EOS5HpZJiBFvDFtXB84bmWQW131DmULfNnojEctADeFwYjBDQV3pk --stake-net "5000.0000 WBI" --stake-cpu "100000.0000 WBI" --buy-ram-kbytes 4
+
+worblim system newaccount eosio --transfer eosiosg11111 EOS6hjg1JPpgSWY5pjEGtt1EWyvBmpgF8B5RfwdvV9kT8XgthqBh6 --stake-net "5000.0000 WBI" --stake-cpu "100000.0000 WBI" --buy-ram-kbytes 4
+
